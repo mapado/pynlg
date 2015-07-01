422b9de8bb9f3ea4a7e48b4f381ba19938499549
Balthazar Rouberol <br@imap.cc>
2015-07-01 17:50:47 +0200

    Project layout

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pynlg",
+    version='0.1',
+    description='Natural Language Generation in Python',
+    maintainer=["Jerry Nieuviarts"],
+    author_email=['jerry@mapado.com'],
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[],
+)
