91638bdb2a626afe9c585938f1f73050f11d9148
Balthazar Rouberol <br@imap.cc>
2015-07-09 16:56:41 +0200

    Small feature tweaks

---
+++ pynlg/lexicon/feature/form.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+"""Definition of form-related features."""
+
+BARE_INFINITIVE = u"bare_infinitive"
+GERUND = u"gerund"
+IMPERATIVE = u"imperative"
+INFINITIVE = u"infinitive"
+NORMAL = u"normal"
+PAST_PARTICIPLE = u"past_participle"
+PRESENT_PARTICIPLE = u"present_participle"
+SUBJUNCTIVE = u"subjunctive"
