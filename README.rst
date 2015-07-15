5bf6dae7ef7750848b9a66295e55b63cf3400100
Balthazar Rouberol <br@imap.cc>
2015-07-15 17:43:27 +0200

    Better define license and setup metadata

---
+++ README.rst
@@ -0,0 +1,42 @@
+pynlg
+=====
+
+``pynlg`` is a pure python re-implementation of `SimpleNLG-EnFr <https://github.com/rali-udem/SimpleNLG-EnFr>`_, a java library enabling bilingual `text surface realisation <https://en.wikipedia.org/wiki/Realization_%28linguistics%29>`_, based on `SimpleNLG <https://github.com/simplenlg/simplenlg>`_.
+
+``pynlg`` supports Python2.7 and 3.4. Python 3.3 should work, but is untested.
+
+Examples
+--------
+
+TODO
+
+Supported languages
+--------------------
+
+For now, the following languages are supported:
+
+- french (in process)
+- english (in process)
+
+How can I contribute?
+---------------------
+
+First, clone the repository, and create a new local branch. Once the new feature is implemented (or the bug is fixed) and everything is **tested** properly, push your work and create a pull request.
+
+Note: you should run the tests beforehand, and make sure they pass:
+
+.. code-block:: bash
+
+    $ tox
+
+
+How do I add support for a new language?
+----------------------------------------
+
+TODO
+
+
+License
+-------
+
+TODO
--- setup.py
+++ setup.py
@@ -4,9 +4,31 @@ setup(
     name="pynlg",
     version='0.1',
     description='Natural Language Generation in Python',
-    maintainer=["Jerry Nieuviarts"],
+    long_description=open('README.rst').read(),
+    author=["Jerry Nieuviarts"],
     author_email=['jerry@mapado.com'],
+    url='http://github.com/mapado/pynlg',
+    license='MIT',
+    # See list of classifiers here:
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Linguistic'
+    ],
     include_package_data=True,
-    packages=find_packages(),
+    packages=find_packages(exclude=['test/*']),
     install_requires=['six'],
+    zip_safe=False,
+    keywords=['text realisation', 'pynlg', 'simplenlg'],
 )
