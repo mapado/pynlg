37226dbd0ee907df1908cc47b7fc0e4088bf12e3
Balthazar Rouberol <br@imap.cc>
2015-07-15 17:53:18 +0200

    Handle version number using bumpversion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pynlg",
-    version='0.1',
+    version='0.1.0',
     description='Natural Language Generation in Python',
     long_description=open('README.rst').read(),
     author=["Jerry Nieuviarts"],
