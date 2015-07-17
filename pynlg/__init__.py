e15728a30fb6b024cd35cf7b06746ec143d86132
Balthazar Rouberol <br@imap.cc>
2015-07-17 12:02:28 +0200

    First draft of a high level API

--- pynlg/__init__.py
+++ pynlg/__init__.py
@@ -1,1 +1,15 @@
+from .spec.phrase import NounPhraseElement
+
 __version__ = '0.1.0'
+
+
+def make_noun_phrase(lexicon, specifier, noun, modifiers=None):
+    phrase = NounPhraseElement(lexicon)
+    phrase.head = noun
+    phrase.specifier = specifier
+    if modifiers:
+        if not isinstance(modifiers, list):
+            modifiers = [modifiers]
+        for modifier in modifiers:
+            phrase.add_modifier(modifier)
+    return phrase
