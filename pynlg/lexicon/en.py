4c02bd13d137fbe1c34790fdf2b81dd3447a8cf6
Balthazar Rouberol <br@imap.cc>
2015-07-17 10:18:38 +0200

    Define a coordination conjuntion for each language/lexicon

--- pynlg/lexicon/en.py
+++ pynlg/lexicon/en.py
@@ -2,8 +2,11 @@
 
 """Definition of the english Lexicon."""
 
+from __future__ import unicode_literals
+
 from .lexicon import Lexicon
 from .lang import ENGLISH
+from .feature.category import CONJUNCTION
 
 
 class EnglishLexicon(Lexicon):
@@ -11,3 +14,7 @@ class EnglishLexicon(Lexicon):
     """Lexicon defining specific rules for the english language."""
 
     language = ENGLISH
+
+    @property
+    def conjunction_coordination(self):
+        return self.first('et', category=CONJUNCTION)
