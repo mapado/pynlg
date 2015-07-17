e27b2ed0b7e9e433828f1451cebe9d3c59f4eec7
Balthazar Rouberol <br@imap.cc>
2015-07-17 10:18:13 +0200

    A PhraseHelper no longer holds a phrase instance

--- pynlg/test/test_helper_fr.py
+++ pynlg/test/test_helper_fr.py
@@ -17,8 +17,12 @@ from ..lexicon.feature.number import SINGULAR, PLURAL
 
 @pytest.fixture
 def noun_helper_fr(lexicon_fr):
-    phrase = PhraseElement(category=NOUN_PHRASE, lexicon=lexicon_fr)
-    return FrenchNounPhraseHelper(phrase=phrase)
+    return FrenchNounPhraseHelper()
+
+
+@pytest.fixture
+def phrase(lexicon_fr):
+    return PhraseElement(category=NOUN_PHRASE, lexicon=lexicon_fr)
 
 
 @pytest.mark.parametrize('person, number, gender, expected', [
@@ -31,11 +35,11 @@ def noun_helper_fr(lexicon_fr):
     (THIRD, PLURAL, MASCULINE, u'ils'),
     (THIRD, PLURAL, FEMININE, u'elles'),
 ])
-def test_create_pronoun(noun_helper_fr, person, number, gender, expected):
-    noun_helper_fr.phrase.person = person
-    noun_helper_fr.phrase.number = number
-    noun_helper_fr.phrase.gender = gender
-    pronoun = noun_helper_fr.create_pronoun()
+def test_create_pronoun(noun_helper_fr, phrase, person, number, gender, expected):
+    phrase.person = person
+    phrase.number = number
+    phrase.gender = gender
+    pronoun = noun_helper_fr.create_pronoun(phrase)
     assert isinstance(pronoun, InflectedWordElement)
     assert pronoun.base_form == expected
 
@@ -55,65 +59,66 @@ def test_is_ordinal(word, expected):
     assert FrenchNounPhraseHelper.is_ordinal(word) is expected
 
 
-def test_add_null_modifier(lexicon_fr, noun_helper_fr, mocker):
+def test_add_null_modifier(lexicon_fr, noun_helper_fr, phrase, mocker):
     mocker.patch.object(PhraseElement, 'add_pre_modifier', autospec=True)
     mocker.patch.object(PhraseElement, 'add_post_modifier', autospec=True)
-    noun_helper_fr.add_modifier(None)
-    assert noun_helper_fr.phrase.add_pre_modifier.call_count == 0
-    assert noun_helper_fr.phrase.add_post_modifier.call_count == 0
+    noun_helper_fr.add_modifier(phrase, modifier=None)
+    assert phrase.add_pre_modifier.call_count == 0
+    assert phrase.add_post_modifier.call_count == 0
 
 
-def test_add_post_modifier_word_element(lexicon_fr, noun_helper_fr):
+def test_add_post_modifier_word_element(lexicon_fr, noun_helper_fr, phrase):
     adj = lexicon_fr.first(u'meilleur', category=ADJECTIVE)
-    noun_helper_fr.add_modifier(adj)
-    assert not noun_helper_fr.phrase.premodifiers
-    assert isinstance(noun_helper_fr.phrase.postmodifiers[0], WordElement)
-    assert noun_helper_fr.phrase.postmodifiers[0].base_form == u'meilleur'
+    noun_helper_fr.add_modifier(phrase, modifier=adj)
+    assert not phrase.premodifiers
+    assert isinstance(phrase.postmodifiers[0], WordElement)
+    assert phrase.postmodifiers[0].base_form == u'meilleur'
 
 
-def test_add_post_modifier_unknown_string_element(lexicon_fr, noun_helper_fr):
+def test_add_post_modifier_unknown_string_element(lexicon_fr, noun_helper_fr, phrase):
     assert u'badass' not in lexicon_fr
-    noun_helper_fr.add_modifier(u'badass')
+    noun_helper_fr.add_modifier(phrase, modifier=u'badass')
     assert u'badass' in lexicon_fr
-    assert isinstance(noun_helper_fr.phrase.postmodifiers[0], WordElement)
-    assert not noun_helper_fr.phrase.premodifiers
-    assert noun_helper_fr.phrase.postmodifiers[0].base_form == u'badass'
+    assert isinstance(phrase.postmodifiers[0], WordElement)
+    assert not phrase.premodifiers
+    assert phrase.postmodifiers[0].base_form == u'badass'
 
 
-def test_add_post_modifier_unknown_complex_string_element(lexicon_fr, noun_helper_fr):
+def test_add_post_modifier_unknown_complex_string_element(
+        lexicon_fr, noun_helper_fr, phrase):
     assert u'totalement badass' not in lexicon_fr
-    noun_helper_fr.add_modifier(u'totalement badass')
+    noun_helper_fr.add_modifier(phrase, modifier=u'totalement badass')
     assert u'totalement badass' not in lexicon_fr
-    assert isinstance(noun_helper_fr.phrase.postmodifiers[0], StringElement)
-    assert not noun_helper_fr.phrase.premodifiers
-    assert noun_helper_fr.phrase.postmodifiers[0].realisation == u'totalement badass'
+    assert isinstance(phrase.postmodifiers[0], StringElement)
+    assert not phrase.premodifiers
+    assert phrase.postmodifiers[0].realisation == u'totalement badass'
 
 
-def test_add_pre_modifier_ordinal_word(lexicon_fr, noun_helper_fr):
+def test_add_pre_modifier_ordinal_word(lexicon_fr, noun_helper_fr, phrase):
     adj = lexicon_fr.first(u'deuxième', category=ADJECTIVE)
-    noun_helper_fr.add_modifier(adj)
-    assert isinstance(noun_helper_fr.phrase.premodifiers[0], WordElement)
-    assert noun_helper_fr.phrase.premodifiers[0].base_form == u'deuxième'
-    assert not noun_helper_fr.phrase.postmodifiers
+    noun_helper_fr.add_modifier(phrase, modifier=adj)
+    assert isinstance(phrase.premodifiers[0], WordElement)
+    assert phrase.premodifiers[0].base_form == u'deuxième'
+    assert not phrase.postmodifiers
 
 
-def test_add_pre_modifier_inflected_word(lexicon_fr, noun_helper_fr):
+def test_add_pre_modifier_inflected_word(lexicon_fr, noun_helper_fr, phrase):
     word = lexicon_fr.first(u'même')  # meme is preposed
     infl = word.inflex(number='plural')
-    noun_helper_fr.add_modifier(infl)
-    assert isinstance(noun_helper_fr.phrase.premodifiers[0], WordElement)
-    assert noun_helper_fr.phrase.premodifiers[0] == infl.base_word
-    assert not noun_helper_fr.phrase.postmodifiers
+    noun_helper_fr.add_modifier(phrase, modifier=infl)
+    assert isinstance(phrase.premodifiers[0], WordElement)
+    assert phrase.premodifiers[0] == infl.base_word
+    assert not phrase.postmodifiers
 
 
 @pytest.mark.parametrize('word', [
     u'deuxième',  # ordinal
     u'même'  # preposed
 ])
-def test_add_pre_modifier_adjective_phrase(lexicon_fr, noun_helper_fr, word):
+def test_add_pre_modifier_adjective_phrase(lexicon_fr, noun_helper_fr, phrase, word):
     adj_phrase = AdjectivePhraseElement(lexicon_fr)
     adj = lexicon_fr.first(word, category=ADJECTIVE)
     adj_phrase.head = adj
-    noun_helper_fr.add_modifier(adj_phrase)
-    assert noun_helper_fr.phrase.premodifiers[0] == adj_phrase
-    assert not noun_helper_fr.phrase.postmodifiers
+    noun_helper_fr.add_modifier(phrase, modifier=adj_phrase)
+    assert phrase.premodifiers[0] == adj_phrase
+    assert not phrase.postmodifiers
